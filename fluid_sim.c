fb4646ea4a492057aaec9d5faaed941d12099ae1
WilliamG99 <19willgarcia99@gmail.com>
2024-12-30 21:35:40 +0000

    add multiple particles, basic interaction with boundary working and start on kernel

--- fluid_sim.c
+++ fluid_sim.c
@@ -1,3 +1,9 @@
+#define GRAVITY 9.81
+#define BOUNCE_DAMPING 0.8
+
+#define SMOOTING_RADIUS 1.0
+#define PARTICLE_MASS 1.0
+
 typedef enum EntityArchetype {
 	arch_nil = 0,
 	arch_particle = 1,
@@ -7,7 +13,7 @@ typedef enum EntityArchetype {
 typedef struct Entity {
 	bool is_valid;
 	EntityArchetype arch;
-	Vector2 pos;
+	Vector2 position;
 	Vector2 velocity;
 	Vector2 acceleration;
 	float mass;
@@ -46,12 +52,13 @@ void entity_destroy(Entity* entity) {
 
 void setup_particle(Entity* en) {
 	en->arch = arch_particle;
+	en->position = v2(0.0, 0.0);
+	en->velocity = v2(0.0, 0.0);
 }
 
 void setup_boundary(Entity* en) {
 	en->arch = arch_boundary;
 }
-
 
 int entry(int argc, char **argv) {
 	window.title = STR("FluidSim");
@@ -67,12 +74,19 @@ int entry(int argc, char **argv) {
 	Entity* particle_en = entity_create();
 	setup_particle(particle_en);
 
+    for (int i = 0; i < 10	; i++) {
+        Entity* en = entity_create();
+        setup_particle(en);
+        en->position = v2(get_random_float32_in_range(-20.0, 20.0), get_random_float32_in_range(-50.0, 50.0));
+		en->velocity = v2(0.0, 0.0);
+    }
+
 	Entity* boundary0_en = entity_create();
 	setup_boundary(boundary0_en);
-	boundary0_en->boundaryPoint0 = v2(-10, -10);
-	boundary0_en->boundaryPoint1 = v2(-10, -11);
-	boundary0_en->boundaryPoint2 = v2( 10, -11);
-	boundary0_en->boundaryPoint3 = v2( 10, -10);
+	boundary0_en->boundaryPoint0 = v2(-50, -50);
+	boundary0_en->boundaryPoint1 = v2( 50, -50);
+	boundary0_en->boundaryPoint2 = v2( 50,  50);
+	boundary0_en->boundaryPoint3 = v2(-50,  50);
 
 	float64 seconds_counter = 0.0;
 	s32 frame_count = 0;
@@ -99,21 +113,52 @@ int entry(int argc, char **argv) {
 				switch (en->arch) {
 
 					case arch_particle:
+						// Particle Updates
+						// Velocity
+						en->velocity = v2_add(en->velocity, v2_mulf(v2(0.0, -GRAVITY), delta_t));
+						// Position
+						en->position = v2_add(en->position, v2_mulf(en->velocity, delta_t));
+
+						// Simple Collision w/ BoundaryFloor
+						if (en->position.y < -50.0) {
+							en->velocity.y = -(en->velocity.y * BOUNCE_DAMPING);
+						}
+
+						// Particle Transform
 						Vector2 size	= v2(1.0,1.0);
 						Matrix4 xform	= m4_scalar(1.0);
-						xform			= m4_translate(xform, v3(particle_en->pos.x, particle_en->pos.y, 0));
-						xform			= m4_translate(xform, v3(size.x * -0.5, 0.0, 0));
+						xform			= m4_translate(xform, v3(en->position.x, en->position.y, 0));
+						xform			= m4_translate(xform, v3(size.x * -0.5, size.y * -0.5, 0));
 						draw_circle_xform(xform, size, COLOR_WHITE);
-						break;
+						
+						// Smoothing Kernel
+						for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+							Entity* en_neighbour = &world->entities[i];
+							if (en_neighbour->is_valid) {
+
+								switch (en_neighbour->arch) {
+
+									case arch_particle:
+										float32 dist = v2_length(v2_sub(en->position, en_neighbour->position));
+										break;
+									
+									default:
+										break;
+								}
+							}
+						}
 
 					case arch_boundary:
-						draw_line(boundary0_en->boundaryPoint0, boundary0_en->boundaryPoint3, 1.0, COLOR_RED);
+						draw_line(en->boundaryPoint0, en->boundaryPoint1, 0.5, COLOR_RED);
+						draw_line(en->boundaryPoint1, en->boundaryPoint2, 0.5, COLOR_RED);
+						draw_line(en->boundaryPoint2, en->boundaryPoint3, 0.5, COLOR_RED);
+						draw_line(en->boundaryPoint3, en->boundaryPoint0, 0.5, COLOR_RED);
 						break;
 
 					default:
 					{
 						// Matrix4 xform = m4_scalar(1.0);
-						// xform         = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+						// xform         = m4_translate(xform, v3(en->position.x, en->position.y, 0));
 						// draw_circle_xform(xform, v2(1, 1), COLOR_WHITE);
 						break;
 					}
@@ -121,31 +166,18 @@ int entry(int argc, char **argv) {
 			}
 		}
 
+		// Window Commands/Keys
 		if (is_key_just_pressed(KEY_ESCAPE)) {
 			window.should_close = true;
 		}
-
-		Vector2 input_axis = v2(0, 0);
-		if (is_key_down('A')) {
-			input_axis.x -= 1.0;
-		}
-		if (is_key_down('D')) {
-			input_axis.x += 1.0;
-		}
-		if (is_key_down('S')) {
-			input_axis.y -= 1.0;
-		}
-		if (is_key_down('W')) {
-			input_axis.y += 1.0;
-		}
-		input_axis = v2_normalize(input_axis);
-		particle_en->pos = v2_add(particle_en->pos, v2_mulf(input_axis, 100.0 * delta_t));
 
 		gfx_update();
 		seconds_counter += delta_t;
 		frame_count += 1;
 		if (seconds_counter > 1.0) {
 			log("fps: %i", frame_count);
+			log("Position: (%f, %f)\n", particle_en->position.x, particle_en->position.y);
+			log("Velocity: (%f, %f)\n", particle_en->velocity.x, particle_en->velocity.y);
 			seconds_counter = 0.0;
 			frame_count = 0;
 		}
